39fe4cf8117a8abfa6c040cc7f57478794d5c125
Emo Onerhime <emo.onerhime@gmail.com>
2025-05-03 16:23:48 +0100

    Fix import path for ListingItem component in SearchClient

--- src/components/SearchClient.js
+++ src/components/SearchClient.js
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import ListingItem from "@/ListingItem";
+import ListingItem from "@/components/ListingItem";
 import { useRouter, useSearchParams } from "next/navigation";
 
 export default function SearchClient() {
